703ff6a476a6f9043e35804512b7643b82c36360
Alejandro Casares M <acasares@puce.edu.ec>
2016-04-30 06:15:44 -0500

    Took of the border of legend in plot4

--- plot4.R
+++ plot4.R
@@ -48,7 +48,7 @@ if (!file.exists("./data/household_power_consumption.txt")) {
         points(t, Sub_metering_2, type = 'l', col = "red",lwd = 2)
         points(t, Sub_metering_3, type = 'l', col = "blue",lwd = 2)
         legend("topright", lwd = 1, cex = 0.7, col = c("gray36", "red", "blue"), legend = c("Sub_metering_1",
-                                            "Sub_metering_2", "Sub_metering_3"))
+                                        "Sub_metering_2", "Sub_metering_3"), bty='n')
         axis(1,labels=c("Thu","Fri","Sat"),at = c(10,1440,2870))
         # Plot [1,2] Datetime vs. Voltage
         plot(t, Voltage, type = 'l',
